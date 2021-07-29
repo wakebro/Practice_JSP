265c66270100a82a2df22b3a26da1c5b877b6189
unknown <wakebro119@naver.com>
2021-07-29 23:53:45 +0900

    20210729

--- src/com/wakebro/web/Nana.java
+++ src/com/wakebro/web/Nana.java
@@ -1,16 +1,18 @@
 package com.wakebro.web;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-public class Nana extends HttpServlet{
-@Override
-protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	PrintWriter out =  resp.getWriter();
-	out.println("Hello~~ ");
-}
+@WebServlet("/hi")
+public class Nana extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("Hello~~ ");
+	}
 }
