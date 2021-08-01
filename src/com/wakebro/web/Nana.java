61ba3e3cd3943efa4e72a39b2a3a36dcbe8f160e
unknown <wakebro119@naver.com>
2021-08-01 16:29:35 +0900

    20210801

--- src/com/wakebro/web/Nana.java
+++ src/com/wakebro/web/Nana.java
@@ -8,11 +8,24 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 @WebServlet("/hi")
 public class Nana extends HttpServlet {
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void service(HttpServletRequest req, 
+						   HttpServletResponse resp) 
+						   throws ServletException, IOException {
+		resp.setCharacterEncoding("utf-8");
+		resp.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = resp.getWriter();
-		out.println("Hello~~ ");
+		int cnt = 100;
+		String temp = req.getParameter("cnt");
+		if(temp!=null && !temp.equals(""))
+			cnt = Integer.parseInt(temp);
+
+
+		for (int i = 0; i <cnt; i++) {
+			out.println((i + 1) + ". Hello~~ 안녕 Servlet!<br>");
+		}
 	}
 }
