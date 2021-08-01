512b3578fe95d9f72574361ae02a15801d6365c0
unknown <wakebro119@naver.com>
2021-08-01 21:02:33 +0900

    20210801

--- src/com/wakebro/web/CalcProcess.java
+++ src/com/wakebro/web/CalcProcess.java
@@ -8,8 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-@WebServlet("/add")
-public class AddProcess extends HttpServlet{
+@WebServlet("/calc")
+public class CalcProcess extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, 
 						   HttpServletResponse resp) 
@@ -20,8 +20,11 @@ public class AddProcess extends HttpServlet{
 		PrintWriter out = resp.getWriter();
 		String x_ = req.getParameter("x");
 		String y_ = req.getParameter("y");
+		String op = req.getParameter("operator");
+		
 		int x = 0;
 		int y = 0;
+		int result = 0;
 
 		if(x_!=null && !x_.equals(""))
 			x = Integer.parseInt(x_);
@@ -29,8 +32,11 @@ public class AddProcess extends HttpServlet{
 		if(y_!=null && !y_.equals(""))
 			y = Integer.parseInt(y_);
 		
-		int result = x + y;
+		if(op.equals("덧셈"))
+			result = x + y;
+		else	
+			result = x - y;
 		
-		out.print("계산 결과 : "+result);
+		out.print("계산 결과 : " + result);
 	}
 }
