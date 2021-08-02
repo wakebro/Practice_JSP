630ef0d6d20cd7c230d9bd40bd7c930ba5a8108c
unknown <wakebro119@naver.com>
2021-08-02 23:05:15 +0900

    20210802

---
+++ src/com/wakebro/web/CalcProcess2.java
@@ -0,0 +1,50 @@
+package com.wakebro.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/calc2")
+public class CalcProcess2 extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, 
+						   HttpServletResponse resp) 
+					throws ServletException, IOException {
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; character=UTF-8");
+		ServletContext application = req.getServletContext();
+		
+		PrintWriter out = resp.getWriter();
+		String v_ = req.getParameter("v");
+		String op = req.getParameter("operator");
+		
+		int v = 0;
+		int result = 0;
+
+		if(!v_.equals("")) v = Integer.parseInt(v_);
+		
+		// 계산
+		if(op.equals("=")) {
+			int x = (Integer)application.getAttribute("value");
+			int y = v;
+			String operator = (String)application.getAttribute("op");
+			if(operator.equals("+"))
+				result = x + y;
+			else	
+				result = x - y;
+			out.print("계산 결과 : " + result);
+		}
+		// 값을 저장
+		else {
+			application.setAttribute("value", v);
+			application.setAttribute("op", op);
+			resp.sendRedirect("calc2.html");
+		}
+		
+	}
+}
