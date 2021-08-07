e124f8857d707ad0431e3f71b2132a2074b03880
unknown <wakebro119@naver.com>
2021-08-07 18:36:06 +0900

    20210807

--- src/com/wakebro/web/CalcProcess2.java
+++ src/com/wakebro/web/CalcProcess2.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,8 +19,13 @@ public class CalcProcess2 extends HttpServlet{
 					throws ServletException, IOException {
 		resp.setCharacterEncoding("UTF-8");
 		resp.setContentType("text/html; character=UTF-8");
-		ServletContext application = req.getServletContext();
-		HttpSession session = req.getSession();
+		
+//		Application and Session
+//		ServletContext application = req.getServletContext();
+//		HttpSession session = req.getSession();
+		
+//		Cookie
+		Cookie[] cookies = req.getCookies();
 		
 		PrintWriter out = resp.getWriter();
 		String v_ = req.getParameter("v");
@@ -32,11 +38,33 @@ public class CalcProcess2 extends HttpServlet{
 		
 		// 계산
 		if(op.equals("=")) {
+			
+//			Application
 //			int x = (Integer)application.getAttribute("value");
-			int x = (Integer)session.getAttribute("value");
+			
+//			Session
+//			int x = (Integer)session.getAttribute("value");
+			
+//			Cookie
+			int x = 0;
+			for(Cookie cookie : cookies) {
+				if(cookie.getName().equals("value")) {
+					x = Integer.parseInt(cookie.getValue());
+					break;
+				}
+			}
+			
+			String operator = "";
+			for(Cookie cookie : cookies) {
+				if(cookie.getName().equals("op")) {
+					operator = cookie.getValue();
+					break;
+				}
+			}
+			
 			int y = v;
 //			String operator = (String)application.getAttribute("op");
-			String operator = (String)session.getAttribute("op");
+//			String operator = (String)session.getAttribute("op");
 			if(operator.equals("+"))
 				result = x + y;
 			else	
@@ -45,11 +73,23 @@ public class CalcProcess2 extends HttpServlet{
 		}
 		// 값을 저장
 		else {
+//			Application
 //			application.setAttribute("value", v);
 //			application.setAttribute("op", op);
-			session.setAttribute("value", v);
-			session.setAttribute("op", op);
-			resp.sendRedirect("calc2.html");
+			
+//			Session
+//			session.setAttribute("value", v);
+//			session.setAttribute("op", op);
+			
+//			resp.sendRedirect("calc2.html");
+			
+			
+//			Cookie
+			Cookie valuecookie = new Cookie("value", String.valueOf(v));
+			Cookie opcookie = new Cookie("op", op);
+			resp.addCookie(valuecookie);
+			resp.addCookie(opcookie);
+			
 		}
 		
 	}
