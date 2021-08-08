8fa6bec14f841a833d04e7a35a842361a23bcc65
unknown <wakebro119@naver.com>
2021-08-08 18:18:39 +0900

    20210808

---
+++ src/com/wakebro/web/CalcProcess3.java
@@ -0,0 +1,74 @@
+package com.wakebro.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+@WebServlet("/calc3")
+public class CalcProcess3 extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, 
+						   HttpServletResponse resp) 
+					throws ServletException, IOException {
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; character=UTF-8");
+		
+//		Application and Session
+//		ServletContext application = req.getServletContext();
+//		HttpSession session = req.getSession();
+		
+//		Cookie
+		Cookie[] cookies = req.getCookies();
+		
+		String value = req.getParameter("value");
+		String operator = req.getParameter("operator");
+		String dot = req.getParameter("dot");
+		
+		String exp = "";
+		if(cookies != null) {
+			for(Cookie cookie : cookies) {
+				if(cookie.getName().equals("exp")) {
+					exp = cookie.getValue();
+					break;
+				}
+			}
+		}
+		System.out.println("calcP3 : "+exp);
+		
+		if(operator != null && operator.equals("=")) {
+			ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+			try {
+				exp = String.valueOf(engine.eval(exp));
+			} catch (ScriptException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else if(operator != null && operator.equals("C")) {
+			exp = "";
+			
+		}
+		else {
+			exp += (value == null) ? "" : value;
+			exp += (operator == null) ? "" : operator;
+			exp += (dot == null) ? "" : dot;
+		}
+		
+		Cookie expCookie = new Cookie("exp", exp);
+		if(operator != null && operator.equals("C")) {
+			expCookie.setMaxAge(0);
+		}
+		resp.addCookie(expCookie);
+		resp.sendRedirect("calcpage");
+	}
+}
